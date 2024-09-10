907c399edd6469fa59e63b7add068e5091686961
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-10 14:58:48 -0400

    output and params

--- F24W2IntroToMethods/Program.cs
+++ F24W2IntroToMethods/Program.cs
@@ -13,15 +13,56 @@
             Console.WriteLine("str = " + str);
             Console.WriteLine("arr[0] = " + arr[0]);
 
-            ChangeValue(num, str, arr);
+            ChangeValue(ref num, str, arr);
 
             Console.WriteLine("\n\nAfter changes:\n");
             Console.WriteLine("num = " + num);
             Console.WriteLine("str = " + str);
             Console.WriteLine("arr[0] = " + arr[0]);
+
+            Console.WriteLine("\n\n");
+
+
+            int a = 4, b = 6;
+            //int sum, mul;
+            Calculate(a, b, out int sum, out int mul);
+
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Mul = " + mul);
+
+            Console.WriteLine("\n\n");
+
+
+
+            Console.WriteLine("a = " + a + " b = " + b);
+            Console.WriteLine($"a = {a} b = {b}");
+            Console.WriteLine("a = {0} b = {1}", a, b);
+
+            Console.WriteLine("\n\n");
+
+
+            PrintArray(arr);
+            PrintArray(4, 3, 4, 5, 65, 76, 8, 76, 5, 4, 3, 32, 65);
         }
 
-        static void ChangeValue(int n, string s, int[] a)
+        static void PrintArray(params int[] a)
+        {
+            Console.WriteLine("\nNumber of items: " + a.Length);
+
+            foreach (int i in a)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+
+
+        static void Calculate(int x, int y, out int sum, out int mul)
+        {
+            sum = x + y;
+            mul = x * y;
+        }
+
+
+        static void ChangeValue(ref int n, string s, int[] a)
         {
             n = n + 100;
             s = "bye";
